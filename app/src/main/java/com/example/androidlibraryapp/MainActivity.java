4b2f59429a19b3e65b3f1c79da39b5506aa9bf06
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2025-07-02 05:53:27 +0300

    Initializing UI components

--- app/src/main/java/com/example/androidlibraryapp/MainActivity.java
+++ app/src/main/java/com/example/androidlibraryapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidlibraryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Identify UI elements
+    private Button btnAllBooks, btnCurrentlyReading, btnWantToRead, btnRead, btnFav, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize Elements
+        initViews();
+
+        // Navigate to other activities
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews(){
+
+        btnAllBooks = findViewById(R.id.btnListAllBooks);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnRead = findViewById(R.id.btnRead);
+        btnFav = findViewById(R.id.btnFavourite);
+        btnAbout = findViewById(R.id.btnAbout);
+
     }
 }
