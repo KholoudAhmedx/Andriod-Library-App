2bfbdf8e9b83d5369ba84ae4fce06cfa487dbded
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2025-07-05 07:07:17 +0300

    Creating the Adapter and List books

--- app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
+++ app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
@@ -7,9 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class AllBooksActivity extends AppCompatActivity {
-
+    RecyclerView booksRecView;
+    BookRecViewAdapter  adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,21 @@ public class AllBooksActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize the recycler view
+
+        // Create an adapter
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this,2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, 499, "Lost in Mecca", "https://m.media-amazon.com/images/I/51Imj-JDfKL._SL1500_.jpg",
+                "Bothayna Elessa", "About loss and the search for meaning.", "Long Description"));
+
+        adapter.setBooks(books);
+
     }
 }
