9e281e1fb381496e9a9eff71b3249ec404c0b6c5
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2025-07-05 09:10:59 +0300

    Adding a New Book Details

--- app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
+++ app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -33,11 +34,13 @@ public class AllBooksActivity extends AppCompatActivity {
         booksRecView = findViewById(R.id.booksRecView);
 
         booksRecView.setAdapter(adapter);
-        booksRecView.setLayoutManager(new GridLayoutManager(this,2));
+        booksRecView.setLayoutManager(new LinearLayoutManager(this));
 
         ArrayList<Book> books = new ArrayList<>();
         books.add(new Book(1, 499, "Lost in Mecca", "https://m.media-amazon.com/images/I/51Imj-JDfKL._SL1500_.jpg",
                 "Bothayna Elessa", "About loss and the search for meaning.", "Long Description"));
+        books.add(new Book(2, 400, "The Apartment Called Freedom", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1525026933i/1652330.jpg",
+                "Ghazi A. Algosaibi", "The novel is set between 1956 and 1961, and tells of the experiences in love and politics of four Bahraini young men studying at university in Cairo.", "Long Description"));
 
         adapter.setBooks(books);
 
