a2baa23fe269bfd69bab3d3dbbb4a6e75dd56b59
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2025-07-12 01:38:43 +0300

    Passing Books' Data via Utils Class Instead of Initializing Manually.

--- app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
+++ app/src/main/java/com/example/androidlibraryapp/AllBooksActivity.java
@@ -35,14 +35,7 @@ public class AllBooksActivity extends AppCompatActivity {
 
         booksRecView.setAdapter(adapter);
         booksRecView.setLayoutManager(new LinearLayoutManager(this));
-
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book(1, 499, "Lost in Mecca", "https://m.media-amazon.com/images/I/51Imj-JDfKL._SL1500_.jpg",
-                "Bothayna Elessa", "About loss and the search for meaning.", "Long Description"));
-        books.add(new Book(2, 400, "The Apartment Called Freedom", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1525026933i/1652330.jpg",
-                "Ghazi A. Algosaibi", "The novel is set between 1956 and 1961, and tells of the experiences in love and politics of four Bahraini young men studying at university in Cairo.", "Long Description"));
-
-        adapter.setBooks(books);
+        adapter.setBooks(Utils.getInstance().getAllBooks());
 
     }
 }
